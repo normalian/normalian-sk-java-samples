c7350a46f96d9b95095865cebc7f69277a520628
Daichi Isami <daisami@microsoft.com>
2024-07-14 00:52:26 -0700

    add audio-to-text method

--- README.md
+++ README.md
@@ -1,9 +1,13 @@
 # normalian-sk-java-samples
 Here are sample codes for Semantic Kernel Java. You have to setup following env variables to run this sample.
 - AZURE_OPENAI_ENDPOINT
-- AZURE_OPENAI_API_KEY
-- AZURE_OPENAI_DEPLOYMENT_NAME
-- AZURE_OPENAI_TTS_DEPLOYMENT_NAME
+- AZURE_OPENAI_API_KEY: API key of Azure OpenAI
+- AZURE_OPENAI_DEPLOYMENT_NAME: ChatCompletion model deployment
+- AZURE_OPENAI_TTS_DEPLOYMENT_NAME: Text To Speech model deployment
+- AZURE_OPENAI_WHISPER_DEPLOYMENT_NAME: Speech to Text model deployment
+
+Note that you have to create TTS model by following an article because you can deploy TTS model on very few regions. 
+https://learn.microsoft.com/en-us/azure/ai-services/openai/concepts/models#whisper-models
 
 # Reference
 - https://github.com/microsoft/semantic-kernel/blob/main/java/README.md
--- src/test/java/com/normalian/skjavasamples/TextAudioTest.java
+++ src/test/java/com/normalian/skjavasamples/TextAudioTest.java
@@ -12,13 +12,16 @@ import org.junit.Test;
 import com.azure.ai.openai.OpenAIAsyncClient;
 import com.azure.ai.openai.OpenAIClientBuilder;
 import com.azure.core.credential.AzureKeyCredential;
+import com.azure.core.util.BinaryData;
 import com.microsoft.semantickernel.services.audio.AudioContent;
+import com.microsoft.semantickernel.services.audio.AudioToTextExecutionSettings;
+import com.microsoft.semantickernel.services.audio.AudioToTextService;
 import com.microsoft.semantickernel.services.audio.TextToAudioExecutionSettings;
 import com.microsoft.semantickernel.services.audio.TextToAudioService;
 
-public class TextToAudioTest {
-    @Test
-    public void createAudio01() throws IOException {
+public class TextAudioTest {
+    // @Test
+    public void textToAudio01() throws IOException {
 
         File audioFile = new File("test-" + UUID.randomUUID() + ".mp3");
 
@@ -63,5 +66,56 @@ public class TextToAudioTest {
 
         // check the file exists
         assertTrue(audioFile.exists());
+    }
+
+    @Test
+    public void audioTotext01() throws IOException {
+
+        File audioFile = new File("test01.mp3");
+
+        String endpoint = System.getenv("AZURE_OPENAI_ENDPOINT");
+        String secretKey = System.getenv("AZURE_OPENAI_API_KEY");
+        String modelID = System.getenv("AZURE_OPENAI_WHISPER_DEPLOYMENT_NAME");
+
+        if (endpoint == null)
+            throw new NullPointerException("AZURE_OPENAI_ENDPOINT is not defined on env variable");
+        if (secretKey == null)
+            throw new NullPointerException("AZURE_OPENAI_API_KEY is not defined on env variable");
+        if (modelID == null)
+            throw new NullPointerException("AZURE_OPENAI_WHISPER_DEPLOYMENT_NAME is not defined on env variable");
+
+        // Create OpenAIClient instance
+        OpenAIAsyncClient client = new OpenAIClientBuilder()
+                .endpoint(endpoint)
+                .credential(new AzureKeyCredential(secretKey))
+                .buildAsyncClient();
+
+        // create TextToSpeech model
+        var textToAudioService = AudioToTextService.builder()
+                .withModelId(modelID)
+                .withOpenAIAsyncClient(client)
+                .build();
+
+        byte[] data = BinaryData.fromFile(audioFile.toPath()).toBytes();
+
+        AudioToTextExecutionSettings executionSettings = AudioToTextExecutionSettings.builder()
+                .withLanguage("en")
+                .withPrompt("sample prompt")
+                .withResponseFormat("json")
+                .withTemperature(0.3)
+                .withFilename(audioFile.getName())
+                .build();
+
+        String text = textToAudioService.getTextContentsAsync(
+                AudioContent.builder()
+                        .withModelId(modelID) // this setting is mandatory even though this looks duplicated
+                        .withData(data)
+                        .build(),
+                executionSettings).block();
+
+        System.out.println(text);
+
+        // Save audio content to a file
+        assertTrue(text != null & text.length() != 0);
     }
 }
